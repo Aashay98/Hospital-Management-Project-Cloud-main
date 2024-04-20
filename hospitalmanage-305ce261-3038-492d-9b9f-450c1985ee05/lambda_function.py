171760a02f1ab4a5924ce61b6363c43f8e6c76a1
Aashay Gandhi <axg71150@ucmo.edu>
2024-04-20 17:13:46 -0500

    first commit

---
+++ hospitalmanage-305ce261-3038-492d-9b9f-450c1985ee05/lambda_function.py
@@ -0,0 +1,33 @@
+##lambda_function
+import logging
+
+import contact_us
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# initializing size of string 
+def lambda_handler(event, context):
+    logger.info('Received message: %s', event)
+    http_method = event.get('httpMethod')
+    
+        
+    path = (event.get('path').split("/")[1]) 
+    print(path)
+   
+    if http_method == 'OPTION':
+        response = {
+            statusCode: 200,
+            headers: {
+                        'Content-Type' : 'application/json',
+                        'Access-Control-Allow-Origin' : '*',
+                        'Allow' : 'GET, OPTIONS, POST,DELETE',
+                        'Access-Control-Allow-Methods' : 'GET, OPTIONS, POST,DELETE',
+                        'Access-Control-Allow-Headers' : '*'
+                }
+                }
+    else:
+        if path == "contactus":
+            response = contact_us.lambda_handler(event, context)
+    
+    logger.info("Response: %s", response)
+    return response
---
+++ hospitalmanage-305ce261-3038-492d-9b9f-450c1985ee05/rds_config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#config file containing credentials for rds mysql instance
+db_username = "admin"
+db_password = "Admin1234"
+db_name = "hospitalmanage"
+db_endpoint = "hospitalmanage.cwseftx9qvmu.us-east-1.rds.amazonaws.com"
